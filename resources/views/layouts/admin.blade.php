45c0e644f72a893e956154489ef7234cbdca0cd3
felipez-maturana <felipe.maturana.g@usach.cl>
2017-06-29 20:29:41 -0400

    version 1.0

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -178,7 +178,7 @@
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="/admin/socio/buscarsocio"><i class="fa fa-circle-o"></i> Consultar por socio</a></li>
+                <li><a href="/admin/socio/buscarsocio"><i class="fa fa-circle-o"></i>Consultar por socio</a></li>
               </ul>
             </li>
             <?php endif; ?>
